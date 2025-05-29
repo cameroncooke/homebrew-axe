28ef2341ecf24ff32274c4a68a821cec3c4dff49
Cameron Cooke <web@cameroncooke.com>
2025-05-29 23:26:31 +0100

    Simplify formula to use single URL/SHA256 for universal binary - Remove architecture-specific conditionals - Use single URL and SHA256 since we build universal binaries - Fixes workflow update logic compatibility

--- Formula/axe.rb
+++ Formula/axe.rb
@@ -3,22 +3,9 @@ class Axe < Formula
   desc "CLI application for macOS to interact with iOS Simulators and Devices via IDB"
   homepage "https://github.com/cameroncooke/AXe"
 
-  # This version will be updated by CI
-  version "1.0.43" # Placeholder, CI will replace this
-
-  on_macos do
-    if Hardware::CPU.arm?
-      # ARM64_URL_PLACEHOLDER
-      url "https://github.com/cameroncooke/AXe/releases/download/v1.0.43/AXe-macOS-v1.0.43.tar.gz" # Placeholder, CI will replace this
-      # ARM64_SHA256_PLACEHOLDER
-      sha256 "f9d6febbc4b7a4f8caa3fecbd963199bf00b0453f434a827eb3d10efa50ffd2f" # SHA256 of empty file
-    else
-      # X86_64_URL_PLACEHOLDER
-      url "https://github.com/cameroncooke/AXe/releases/download/v1.0.43/AXe-macOS-v1.0.43.tar.gz" # Placeholder, CI will replace this
-      # X86_64_SHA256_PLACEHOLDER
-      sha256 "f9d6febbc4b7a4f8caa3fecbd963199bf00b0453f434a827eb3d10efa50ffd2f" # SHA256 of empty file
-    end
-  end
+  version "1.0.43"
+  url "https://github.com/cameroncooke/AXe/releases/download/v1.0.43/AXe-macOS-v1.0.43.tar.gz"
+  sha256 "f9d6febbc4b7a4f8caa3fecbd963199bf00b0453f434a827eb3d10efa50ffd2f"
 
   depends_on macos: :sonoma 
 
